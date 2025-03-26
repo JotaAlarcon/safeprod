5d347f832e7940831cf45e3c5756b2f710f8b092
JotaAlarcon <alarcon.js@gmail.com>
2025-03-26 20:37:52 -0300

    Actualizacion de app_theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,13 +4,11 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     scaffoldBackgroundColor: Color(0xffE2E9EB),
-    appBarTheme: AppBarTheme(
-      backgroundColor: Color(0xffE2E9EB)
-    )
+    appBarTheme: AppBarTheme(backgroundColor: Color(0xffE2E9EB)),
   );
 
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    scaffoldBackgroundColor: Color(0xff363636)
+    scaffoldBackgroundColor: Color(0xff363636),
   );
 }
