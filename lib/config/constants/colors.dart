a91889757fd16b89237da2482a8247191a92c2ed
unknown <alarcon.js@gmail.com>
2025-03-26 10:42:28 -0300

    se agrega constante de colores

---
+++ lib/config/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color backgroundLight = Color(0xffE2E9EB);
+  static Color primaryLight = Color(0xff3D3377);
+  static Color secondaryLight = Color(0xff68E8E8);
+  static Color greenAlert = Color(0xff00CC2D);
+  static Color redAlert = Color(0xffCC0000);
+  static Color orangeAlert = Color(0xffFFB800);
+  static Color grey = Color(0xff616163);
+  static Color lightGrey = Color(0xffCCCCCC);
+  static Color black = Color(0xff000000);
+  static Color backgroundDark = Color(0xff363636);
+
+}
--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:safejob/config/constants/colors.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    scaffoldBackgroundColor: Color(0xffE2E9EB),
+    scaffoldBackgroundColor: CustomColors.backgroundLight,
     appBarTheme: AppBarTheme(
-      backgroundColor: Color(0xffE2E9EB)
+      backgroundColor: CustomColors.backgroundLight,
     )
   );
 
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    scaffoldBackgroundColor: Color(0xff363636)
+    scaffoldBackgroundColor: CustomColors.backgroundDark,
+    appBarTheme: AppBarTheme(
+      backgroundColor: CustomColors.backgroundDark,
+    )
   );
 }
